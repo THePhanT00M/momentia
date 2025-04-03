50c3996a441bf2d384c49498c353ddd45aff39f1
지유민 <yuminji@stunitas.com>
2025-04-03 09:28:09 +0900

    회원가입

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -32,12 +32,12 @@ export default function Register() {
     };
 
     return (
-        <AuthLayout title="Create an account" description="Enter your details below to create your account">
-            <Head title="Register" />
+        <AuthLayout title="Momentia 계정 생성" description="친구들의 사진과 동영상을 보려면 가입하세요.">
+            <Head title="가입하기" />
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
                     <div className="grid gap-2">
-                        <Label htmlFor="name">Name</Label>
+                        <Label htmlFor="name">성명</Label>
                         <Input
                             id="name"
                             type="text"
@@ -48,13 +48,13 @@ export default function Register() {
                             value={data.name}
                             onChange={(e) => setData('name', e.target.value)}
                             disabled={processing}
-                            placeholder="Full name"
+                            placeholder="ex:) 홍길동"
                         />
                         <InputError message={errors.name} className="mt-2" />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
+                        <Label htmlFor="email">이메일 주소</Label>
                         <Input
                             id="email"
                             type="email"
@@ -64,13 +64,13 @@ export default function Register() {
                             value={data.email}
                             onChange={(e) => setData('email', e.target.value)}
                             disabled={processing}
-                            placeholder="email@example.com"
+                            placeholder="ex:) email@example.com"
                         />
                         <InputError message={errors.email} />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="password">Password</Label>
+                        <Label htmlFor="password">비밀번호</Label>
                         <Input
                             id="password"
                             type="password"
@@ -80,13 +80,13 @@ export default function Register() {
                             value={data.password}
                             onChange={(e) => setData('password', e.target.value)}
                             disabled={processing}
-                            placeholder="Password"
+                            placeholder="비밀번호"
                         />
                         <InputError message={errors.password} />
                     </div>
 
                     <div className="grid gap-2">
-                        <Label htmlFor="password_confirmation">Confirm password</Label>
+                        <Label htmlFor="password_confirmation">비밀번호 재입력</Label>
                         <Input
                             id="password_confirmation"
                             type="password"
@@ -96,21 +96,21 @@ export default function Register() {
                             value={data.password_confirmation}
                             onChange={(e) => setData('password_confirmation', e.target.value)}
                             disabled={processing}
-                            placeholder="Confirm password"
+                            placeholder="비밀번호 재입력"
                         />
                         <InputError message={errors.password_confirmation} />
                     </div>
 
                     <Button type="submit" className="mt-2 w-full" tabIndex={5} disabled={processing}>
                         {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                        Create account
+                        가입
                     </Button>
                 </div>
 
                 <div className="text-muted-foreground text-center text-sm">
-                    Already have an account?{' '}
+                    계정이 있으신가요?{' '}
                     <TextLink href={route('login')} tabIndex={6}>
-                        Log in
+                        로그인
                     </TextLink>
                 </div>
             </form>
